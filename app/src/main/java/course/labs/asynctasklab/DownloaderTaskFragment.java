b0935742d66c132863ea5fc0a96a801b067925a5
Francisco Egloff <fegloff@gmail.com>
2018-09-12 22:18:06 -0500

    Great example of AsynTask, Headless Fragments

---
+++ app/src/main/java/course/labs/asynctasklab/DownloaderTaskFragment.java
@@ -0,0 +1,171 @@
+package course.labs.asynctasklab;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+
+public class DownloaderTaskFragment extends Fragment {
+
+	private DownloadFinishedListener mCallback;
+	private Context mContext;
+
+    public static final ArrayList<Integer> sRawTextFeedIds = new ArrayList<Integer>(
+            Arrays.asList(R.raw.tswift, R.raw.rblack, R.raw.lgaga));
+
+	@SuppressWarnings ("unused")
+	private static final String TAG = "Lab-Threads";
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		// Preserve across reconfigurations
+		setRetainInstance(true);
+		
+		// TODO: Create new DownloaderTask that "downloads" data
+		DownloaderTask downloaderTask = new DownloaderTask();
+		
+		// TODO: Retrieve arguments from DownloaderTaskFragment
+        // Prepare them for use with DownloaderTask.
+        Bundle args = this.getArguments();
+        Integer[] mFeeds = toIntArray(args.getIntegerArrayList(MainActivity.TAG_FRIEND_RES_IDS));
+
+		// TODO: Start the DownloaderTask
+
+		downloaderTask.execute(mFeeds);
+        
+
+	}
+
+	// Assign current hosting Activity to mCallback
+	// Store application context for use by downloadTweets()
+	@Override
+	public void onAttach(Activity activity) {
+		super.onAttach(activity);
+
+		mContext = activity.getApplicationContext(); 
+
+		// Make sure that the hosting activity has implemented
+		// the correct callback interface.
+		try {
+			mCallback = (DownloadFinishedListener) activity;
+		} catch (ClassCastException e) {
+			throw new ClassCastException(activity.toString()
+					+ " must implement DownloadFinishedListener");
+		}
+	}
+
+	// Null out mCallback
+	@Override
+	public void onDetach() {
+		super.onDetach();
+		mCallback = null;
+	}
+
+	// TODO: Implement an AsyncTask subclass called DownLoaderTask.
+	// This class must use the downloadTweets method (currently commented
+	// out). Ultimately, it must also pass newly available data back to 
+	// the hosting Activity using the DownloadFinishedListener interface.
+
+	public class DownloaderTask extends AsyncTask<Integer,Integer,String[]> {
+
+
+		public DownloaderTask() {
+			super();
+		}
+
+		@Override
+		protected void onPostExecute(String[] s) {
+			super.onPostExecute(s);
+			mCallback.notifyDataRefreshed(s);
+		}
+
+		@Override
+		protected String[] doInBackground(Integer... params) {
+            Log.i(TAG, "In do in background");
+            return downloadTweets(params);
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+    }
+
+
+
+    Integer[] toIntArray(List<Integer> list) {
+        Integer[] ret = new Integer[list.size()];
+        for(int i = 0;i < ret.length;i++)
+            ret[i] = list.get(i);
+        return ret;
+    }
+    
+    
+    
+    
+    
+        // TODO: Uncomment this helper method
+		// Simulates downloading Twitter data from the network
+
+
+         private String[] downloadTweets(Integer resourceIDS[]) {
+			final int simulatedDelay = 2000;
+			String[] feeds = new String[resourceIDS.length];
+			try {
+				for (int idx = 0; idx < resourceIDS.length; idx++) {
+					InputStream inputStream;
+					BufferedReader in;
+					try {
+						// Pretend downloading takes a long time
+						Thread.sleep(simulatedDelay);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+
+					inputStream = mContext.getResources().openRawResource(
+							resourceIDS[idx]);
+					in = new BufferedReader(new InputStreamReader(inputStream));
+
+					String readLine;
+					StringBuffer buf = new StringBuffer();
+
+					while ((readLine = in.readLine()) != null) {
+						buf.append(readLine);
+					}
+
+					feeds[idx] = buf.toString();
+
+					if (null != in) {
+						in.close();
+					}
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			return feeds;
+		}
+
+
+
+    
+    
+    
+    
+    
+    
+
+}
